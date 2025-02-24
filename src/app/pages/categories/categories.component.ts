0deb996910f3881be16d5d07c9c1ae2e6c5f5ae8
Daniel Schmitz <danieljfa@gmail.com>
2025-02-24 20:38:47 -0300

    new & edit category

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -19,6 +19,7 @@ import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { Category, CategoryService } from './category.service';
 import { Column } from '../../shared/model';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-categories',
@@ -43,12 +44,19 @@ import { Column } from '../../shared/model';
     ConfirmDialogModule
   ],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
+  providers: [MessageService]
 })
 export class CategoriesComponent implements OnInit {
+  service = inject(CategoryService);
+  message = inject(MessageService);
   categories = signal<Category[]>([]);
   selectedCategories: Category[] = [];
-  service = inject(CategoryService);
+  category: Category = {
+    name: ''
+  };
+  categoryDialog: boolean = false;
+  submitted: boolean = false;
   cols: Column[] = [
     { field: 'id', header: 'Id', customExportHeader: 'id' },
     { field: 'name', header: 'name' }
@@ -56,18 +64,77 @@ export class CategoriesComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  private fetchCategories() {
     this.service.getAll().subscribe((categories) => {
       this.categories.set(categories);
     });
   }
+
   onGlobalFilter(table: Table, event: Event) {
     table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
   }
 
-  deleteCategory(_t44: any) {
+  newCategory() {
+    this.category = { name: '' };
+    this.submitted = false;
+    this.categoryDialog = true;
+  }
+
+  saveCategory() {
+    this.submitted = true;
+    if (this.category.name.trim()) {
+      if (this.category.id) {
+        this.service.update(this.category.id, this.category)
+          .subscribe({
+            next: () => {
+              this.categoryDialog = false;
+              this.submitted = false;
+              this.fetchCategories();
+            },
+            error: (err) => {
+              console.error('Error updating category:', err);
+            },
+            complete: () => {
+              console.log('Category update completed');
+            }
+          });
+      } else {
+        this.service.create(this.category)
+          .subscribe({
+            next: () => {
+              this.categoryDialog = false;
+              this.submitted = false;
+              this.category = { name: '' };
+              this.fetchCategories();
+            },
+            error: (error) => {
+              this.message.add({ severity: 'error', summary: 'Error', detail: error.error.message });
+            },
+            complete: () => {
+              console.log('Category creation completed');
+            }
+          });
+      }
+
+    }
+  }
+
+  deleteCategory(item: Category) {
     throw new Error('Method not implemented.');
   }
-  editCategory(_t44: any) {
-    throw new Error('Method not implemented.');
+
+  editCategory(item: Category) {
+    this.category = { id: item.id, name: item.name };
+    this.submitted = false;
+    this.categoryDialog = true;
   }
+
+  closeDialog() {
+    this.categoryDialog = false;
+    this.submitted = false;
+  }
+
 }
